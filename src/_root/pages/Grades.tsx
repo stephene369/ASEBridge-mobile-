295ff89e8f3b5ca3b22402e9374a77fe75e8ccc5
stephene369 <stephenew36@gmail.com>
2024-11-13 22:38:32 +0100

    Version-2024/11/13-22:38:32

--- src/_root/pages/Grades.tsx
+++ src/_root/pages/Grades.tsx
@@ -1,32 +1,42 @@
 import { Button } from '@/components/ui/button'
-import { Loader } from 'lucide-react'
 import React from 'react'
-import { Filesystem, Directory } from '@capacitor/filesystem';
+import { useUserContext } from "../../context/AuthContext";
+import { json } from 'stream/consumers';
 
-const fetchAndSaveCSV = async () => {
+
+const fetchData = async (userId: any) => {
   try {
-    const response = await fetch('https://www.stats.govt.nz/assets/Uploads/Business-operations-survey/Business-operations-survey-2022/Download-data/business-operations-survey-2022-business-finance.csv');
-    if (!response.ok) {
-      throw new Error('Erreur lors du téléchargement du fichier CSV.');
-    }
+    const response = await fetch('https://cloud.appwrite.io/v1/storage/buckets/6734f75c0029042ea147/files/exemple/view?project=66dc9b3c003c275c9bda&project=66dc9b3c003c275c9bda&mode=admin');
+    const data = await response.text();
 
-    const csvData = await response.text();
-
-    // Enregistrer le fichier localement avec Capacitor
-    await Filesystem.writeFile({
-      path: 'data.csv',
-      data: csvData,
-      directory: Directory.Data
+    // Conversion du CSV en tableau
+    const rows = data.split('\n').map(row => row.split(','));
+    
+    // Extraction des en-têtes et des lignes de données
+    const headers = rows[0]; // Première ligne (en-têtes)
+    const jsonData = rows.slice(1).map(row => {
+      return headers.reduce((acc, header, index) => {
+        acc[header.trim()] = row[index]?.trim(); // Création d'un objet clé-valeur
+        return acc;
+      }, {});
     });
 
-    console.log('Fichier CSV téléchargé et enregistré avec succès.');
+    // Filtrage par userId
+    const filteredData = jsonData.filter(item => item['ID'] === userId);
+    
+    console.log('Données filtrées correspondant à userId:', filteredData);
+
+    console.log('Fichier CSV téléchargé et traité avec succès.');
   } catch (error) {
-    console.error('Erreur lors du téléchargement ou de la sauvegarde du fichier CSV :', error);
-  }
+    console.error('Erreur lors du téléchargement ou du traitement du fichier CSV :', error);
+  } ; 
+
 };
 
 
 function Grades() {
+    const { user } = useUserContext();
+
     return (
         <div className="saved-container">
             <div className="flex gap-2 w-full max-w-5xl">
@@ -40,14 +50,12 @@ function Grades() {
             <div className=''>
                 <Button type="button" 
                     className="h-12 bg-dark-4 px-5 text-light-1 flex-center gap-2 rounded-lg hover:bg-neutral-500"
-                    onClick={()=>fetchAndSaveCSV()}
+                    onClick={()=>fetchData(user.id)}
                     >
                     Update
                 </Button>
 
-
             </div>
-
         </div>
     )
 }
